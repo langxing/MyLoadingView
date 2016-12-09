973ce3074ccb24620b3c6f64088a08e44d7fa3b1
唐杨凯 <tangyangkai@diqi.sh>
2016-12-09 18:13:23 +0800

    CirlceColorView,CirlceMoveView,

--- app/src/main/java/tyk/myloadingview/Activity/MainActivity.java
+++ app/src/main/java/tyk/myloadingview/Activity/MainActivity.java
@@ -1,16 +1,21 @@
 package tyk.myloadingview.Activity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import tyk.myloadingview.R;
-import tyk.myloadingview.View.FirstView;
+import tyk.myloadingview.View.CircleColorView;
+import tyk.myloadingview.View.CircleMoveView;
 
 public class MainActivity extends AppCompatActivity {
     private Button startBtn, endBtn;
-    private FirstView firstView;
+    private CircleColorView circleColorView;
+    private CircleMoveView circleMoveView;
+    private ObjectAnimator rotate;
 
 
     @Override
@@ -21,19 +26,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        firstView= (FirstView) findViewById(R.id.first_view);
+        circleColorView = (CircleColorView) findViewById(R.id.first_view);
+        circleMoveView = (CircleMoveView) findViewById(R.id.second_view);
         startBtn = (Button) findViewById(R.id.start_btn);
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                firstView.startAnimation();
+                circleColorView.startAnimation();
+                circleMoveView.startAnimation();
+
+
+                rotate = ObjectAnimator.ofFloat(circleMoveView, "rotation", 0f, 360f);
+                rotate.setRepeatCount(ValueAnimator.INFINITE);
+                rotate.setDuration(1000);
+                rotate.start();
+
             }
         });
         endBtn = (Button) findViewById(R.id.end_btn);
         endBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                firstView.endAnimation();
+                circleColorView.endAnimation();
+                circleMoveView.endAnimation();
+                rotate.end();
             }
         });
     }
--- app/src/main/java/tyk/myloadingview/View/CircleColorView.java
+++ app/src/main/java/tyk/myloadingview/View/CircleColorView.java
@@ -1,17 +1,14 @@
 package tyk.myloadingview.View;
 
+import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.os.Handler;
-import android.os.Message;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
-import android.view.animation.LinearInterpolator;
 
 import tyk.myloadingview.R;
 
@@ -19,42 +16,38 @@ import tyk.myloadingview.R;
  * Created by tangyangkai on 2016/12/8.
  */
 
-public class FirstView extends View {
+public class CircleColorView extends View {
 
 
     private int firstColor, secondColor;
-    private int mWidth, mHeight, mstartWidth;
+    private int mWidth, mHeight;
     private Paint mPaint;
-    private Thread thread;
-    private Handler handler;
-    private final int START_FLAG = 0;
-    private final int END_FLAG = 1;
-    private int nubmer = 0;
+    private int number = 0;
     private int mRadius;
     private ValueAnimator valueAnimator;
 
-    public FirstView(Context context) {
+    public CircleColorView(Context context) {
         this(context, null);
     }
 
-    public FirstView(Context context, AttributeSet attrs) {
+    public CircleColorView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public FirstView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public CircleColorView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         //获取我们自定义的样式属性
-        TypedArray array = context.getTheme().obtainStyledAttributes(attrs, R.styleable.FirstView, defStyleAttr, 0);
+        TypedArray array = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CircleColorView, defStyleAttr, 0);
         int n = array.getIndexCount();
         for (int i = 0; i < n; i++) {
             int attr = array.getIndex(i);
             switch (attr) {
 
-                case R.styleable.FirstView_firstColor:
+                case R.styleable.CircleColorView_firstColor:
                     // 默认颜色设置为黑色
                     firstColor = array.getColor(attr, Color.BLACK);
                     break;
-                case R.styleable.FirstView_secondColor:
+                case R.styleable.CircleColorView_secondColor:
                     secondColor = array.getColor(attr, Color.BLACK);
                     break;
 
@@ -68,26 +61,6 @@ public class FirstView extends View {
     private void init() {
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
-
-        handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                super.handleMessage(msg);
-
-                if (msg.what == START_FLAG) {
-                    nubmer++;
-                    if (nubmer >= 3) {
-                        nubmer = 0;
-                    }
-                    invalidate();
-                    handler.sendEmptyMessageDelayed(START_FLAG, 500);
-                } else {
-                    handler.removeMessages(START_FLAG);
-                }
-
-            }
-        };
-
 
     }
 
@@ -119,17 +92,19 @@ public class FirstView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        mstartWidth = mWidth / 2 - mHeight * 2;
-        mPaint.setColor(firstColor);
+
+        //画布原点移到中心
+        canvas.translate(mWidth / 2, mHeight / 2);
         mPaint.setStyle(Paint.Style.FILL);
         for (int i = 0; i < 3; i++) {
-            if (i == nubmer) {
+            if (i == number % 3) {
                 mPaint.setColor(secondColor);
+                canvas.drawCircle((i - 1) * mHeight * 2, 0, mRadius, mPaint);
             } else {
                 mPaint.setColor(firstColor);
+                canvas.drawCircle((i - 1) * mHeight * 2, 0, mHeight / 2, mPaint);
             }
-            canvas.drawCircle(mstartWidth, mHeight / 2, mRadius, mPaint);
-            mstartWidth += mHeight * 2;
+
         }
 
 
@@ -137,59 +112,55 @@ public class FirstView extends View {
 
 
     public void startAnimation() {
-//        thread = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                Message message = new Message();
-//                message.what = START_FLAG;
-//                handler.sendMessage(message);
-//            }
-//        });
-//        thread.start();
 
-
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                Message message = new Message();
-                message.what = START_FLAG;
-                handler.sendMessage(message);
-            }
-        });
 
         valueAnimator = ValueAnimator.ofInt(mHeight / 2, mHeight / 8, mHeight / 2);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 mRadius = (int) valueAnimator.getAnimatedValue();
+                invalidate();
+            }
+
+        });
+        //动画循环执行
+        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        valueAnimator.setDuration(500);
+        valueAnimator.start();
+
+        valueAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                number++;
             }
         });
-        valueAnimator.setDuration(1000);
-        valueAnimator.setInterpolator(new LinearInterpolator());
-        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        valueAnimator.start();
 
     }
 
     public void endAnimation() {
-//        thread = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                Message message = new Message();
-//                message.what = END_FLAG;
-//                handler.sendMessage(message);
-//            }
-//        });
-//        thread.start();
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                Message message = new Message();
-                message.what = END_FLAG;
-                handler.sendMessage(message);
-            }
-        });
+
+
+        //cancel结束时保留当前动画值
+        valueAnimator.cancel();
+
+        //end结束时会计算最终值
         valueAnimator.end();
+
     }
 
     @Override
